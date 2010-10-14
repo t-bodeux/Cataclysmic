6191e5a363e797c250edaf7e0db286529469ff45
Bodeux Thibault <t.bodeux@gmail.com>
2010-10-15 00:13:35 +0200

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cataclysmic::Application.config.secret_token = '91edb981e24a93794d4a3b7b7284d1c62bbc5b45dad48b792c5c7dcb31d53ee4e4ff85941cad115cb47333e5d1ce42d2e8720692e27bc8087f772a16d564189a'
